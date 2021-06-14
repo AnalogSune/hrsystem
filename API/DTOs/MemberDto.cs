a706f738363023910a377e2d243d33ea83c8ec0a
AnalogSune <sune_analog@yahoo.gr>
2021-06-14 18:49:23 +0300

    Equals & UserRepositoryTests.cs

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -35,5 +35,57 @@ namespace API.DTOs
         public int RoleId { get; set; }
 
         public bool IsAdmin { get; set; }
+
+        private bool Equals(MemberDto other)
+        {
+            return Id == other.Id && 
+                   Email == other.Email && 
+                   PictureUrl == other.PictureUrl && 
+                   PictureId == other.PictureId && 
+                   DateOfBirth.Equals(other.DateOfBirth) && 
+                   FName == other.FName && 
+                   LName == other.LName && 
+                   Address == other.Address && 
+                   Country == other.Country && 
+                   Nationality == other.Nationality && 
+                   PhoneNumber == other.PhoneNumber && 
+                   DaysOffLeft.Equals(other.DaysOffLeft) && 
+                   Equals(InDepartment, other.InDepartment) && 
+                   Equals(Role, other.Role) && 
+                   DepartmentId == other.DepartmentId && 
+                   RoleId == other.RoleId && 
+                   IsAdmin == other.IsAdmin;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((MemberDto) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = new HashCode();
+            hashCode.Add(Id);
+            hashCode.Add(Email);
+            hashCode.Add(PictureUrl);
+            hashCode.Add(PictureId);
+            hashCode.Add(DateOfBirth);
+            hashCode.Add(FName);
+            hashCode.Add(LName);
+            hashCode.Add(Address);
+            hashCode.Add(Country);
+            hashCode.Add(Nationality);
+            hashCode.Add(PhoneNumber);
+            hashCode.Add(DaysOffLeft);
+            hashCode.Add(InDepartment);
+            hashCode.Add(Role);
+            hashCode.Add(DepartmentId);
+            hashCode.Add(RoleId);
+            hashCode.Add(IsAdmin);
+            return hashCode.ToHashCode();
+        }
     }
 }
--- HrSystemTests/UserRepositoryTests.cs
+++ HrSystemTests/UserRepositoryTests.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Helper;
 using API.Interfaces;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xunit;
@@ -11,33 +17,54 @@ namespace HrSystemTests
 {
     public class UserRepositoryTests
     {
-                    
-        public static Guid ToGuid(int value)
-        {
-            byte[] bytes = new byte[16];
-            BitConverter.GetBytes(value).CopyTo(bytes, 0);
-            return new Guid(bytes);
-        }
-        
-        private readonly UserRepository _sut;
-        private readonly Mock<DataContext> _contextMock = new Mock<DataContext>();
-        private readonly Mock<IMapper> _mapperMock = new Mock<IMapper>();
-        private readonly Mock<IUserRepository> _userRepoMock = new Mock<IUserRepository>();
+        private IMapper Mapper { get; }
+        private readonly UserRepository _repo;
 
         public UserRepositoryTests()
         {
-            var usersMock = _userRepoMock.Object;
-            var mapperMock = _mapperMock.Object;
-            var contextMock = _contextMock.Object;
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new AutoMapperProfiles());
+            });
+            
+            Mapper = mappingConfig.CreateMapper();
+            
+            var options = new DbContextOptionsBuilder<DataContext>()  
+                .UseInMemoryDatabase("InMemoryDb")  
+                .Options;  
+            var dbContext = new DataContext(options);
 
-            _sut = new UserRepository(contextMock, mapperMock);
+            dbContext.Users.AddRangeAsync(
+                new AppUser() {Id = 1, Email = "sune_analog@yahoo.gr", FName = "Kostas", LName = "Ang"},
+                new AppUser() {Id = 2, Email = "user2", FName = "user2", LName = "user2"},
+                new AppUser() {Id = 3, Email = "user3", FName = "user3", LName = "user3"}
+            );
+            
+            dbContext.SaveChangesAsync();
+            _repo = new UserRepository(dbContext, Mapper);
         }
         
         [Fact]
         public async Task getUser_ShouldReturnUser_IfExists()
         {
             //Arrange
+
+            var expectedUser = new MemberDto()
+            {
+                Id = 2,
+                Email = "user2",
+                FName = "user2",
+                LName = "user2"
+            };
             
+            //Act
+
+            var result = await _repo.GetUser(2);
+            
+            //Assert
+            
+            Assert.NotNull(result);
+            Assert.Equal(result, expectedUser);
         }
     }
 }
